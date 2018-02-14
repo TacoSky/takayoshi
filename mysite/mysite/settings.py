160d857545716784733561cb7a97c26fb00add63
root <root@ip-172-31-13-77.ap-northeast-1.compute.internal>
2018-02-14 04:42:22 +0000

    add app for website

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'dhorg$tz3un&l86948&+o=@ftxqdj&zq=air0=+6+@feu!1i=9'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['takayoshi.work']
 
