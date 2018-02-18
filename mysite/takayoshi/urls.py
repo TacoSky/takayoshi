eb6ec6c9f9a1ed31037ae973daa002f0908d44a9
TAYA <waiukigroup@gmail.com>
2018-02-17 21:26:15 -0500

    Design and pages

--- mysite/takayoshi/urls.py
+++ mysite/takayoshi/urls.py
@@ -4,6 +4,10 @@ from . import views
 
 urlpatterns = [
     url(r'^$', views.index, name='index'),
+    url(r'^qualities/$', views.qualities, name='qualities'),
+    url(r'^education/$', views.education, name='education'),
+    url(r'^experiences/$', views.experiences, name='experiences'),
+    url(r'^portfolio/$', views.portfolio, name='portfolio'),
 ]
 
 
--- mysite/takayoshi/views.py
+++ mysite/takayoshi/views.py
@@ -9,3 +9,25 @@ def index(request):
     return render(request,'index.html')
 
 
+def qualities(request):
+    """qualities page"""
+    return render(request,'qualities.html')
+
+
+def education(request):
+    """education page"""
+    return render(request,'education.html')
+
+
+def experiences(request):
+    """experiences page"""
+    return render(request,'experiences.html')
+
+
+
+def portfolio(request):
+    """portfolio page"""
+    return render(request,'portfolio.html')
+
+
+
