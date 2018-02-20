cb0cff6e5e5936cf9033f1a76decf91dddabd5be
TAYA <waiukigroup@gmail.com>
2018-02-20 18:16:05 -0500

    Add DB

--- mysite/takayoshi/admin.py
+++ mysite/takayoshi/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
 
 # Register your models here.
+
+from .models import Company,JobeRole,Experience
+
+admin.site.register(Company)
+admin.site.register(JobeRole)
+admin.site.register(Experience)
+
+class CompanyAdmin(admin.ModelAdmin):
+    list_display = ('name', 'description')
+
--- mysite/takayoshi/models.py
+++ mysite/takayoshi/models.py
@@ -2,4 +2,42 @@ from __future__ import unicode_literals
 
 from django.db import models
 
-# Create your models here.
+class Company(models.Model):
+    name = models.CharField(max_length=64,primary_key=True)
+    startdate = models.DateField()
+    enddate = models.DateField()
+    place = models.CharField(max_length=64)
+    url = models.CharField(max_length=256)
+    description = models.CharField(max_length=1024)
+    def __str__(self):
+        return u'%s' % (self.name)
+
+
+class JobeRole(models.Model):
+    name = models.CharField(max_length=64)
+    company = models.ForeignKey('Company')
+    startdate = models.DateField()
+    enddate = models.DateField()
+    description = models.CharField(max_length=1024)
+    def __str__(self):
+        return u'%s' % (self.name)
+
+class Experience(models.Model):
+    name = models.CharField(max_length=64)
+    role = models.ForeignKey('JobeRole')
+    type = models.CharField(max_length=64)
+    incharge = models.CharField(max_length=256)
+    achievement = models.CharField(max_length=256,blank=True)
+    keyword = models.CharField(max_length=256)
+    achievement = models.CharField(max_length=256,blank=True)
+    url = models.CharField(max_length=256,blank=True)
+    description = models.CharField(max_length=1024)
+    def __str__(self):
+        return u'%s' % (self.name)
+
+
+
+
+
+
+
